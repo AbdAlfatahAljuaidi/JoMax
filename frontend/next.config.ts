99ac59222f00e10046ddf49a8d248c67fa13b172
MRERROR2050 <abdalfatah.aljuaidi@gmail.com>
2026-04-28 08:36:53 +0300

    d

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
+    unoptimized: true,
   },
 };
 
